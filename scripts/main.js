404e15f39422d7182691260617d79d860c03ba1d
manuelcolombia <manuelortizsenior@gmail.com>
2023-01-07 12:44:27 -0500

    Adding Java stuff with the two working buttons and some formatting changed

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,15 @@ myImage.onclick = () => {
     myImage.setAttribute("src", "firefox_logo.png");
   }
 };
+var button = document.getElementById("hidebutton");
+var image = document.getElementById("firefox");
+
+button.addEventListener("click", function() {
+  image.style.display = "none";
+});
+var button = document.getElementById("showbutton");
+var image = document.getElementById("firefox");
+
+button.addEventListener("click", function() {
+  image.style.display = "block";
+});
